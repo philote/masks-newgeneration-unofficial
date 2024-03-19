578ae3449695a3e17e3dbbbf501ed67b6f4c7673
ephson <727468+philote@users.noreply.github.com>
2024-03-19 16:58:23 -0600

    Finished the influences tab and did a little more css cleanup

--- scripts/masks.mjs
+++ scripts/masks.mjs
@@ -37,8 +37,8 @@ Hooks.once("init", () => {
 Hooks.on('setup', () => {
     // Ensure template is loaded so that it will be ready when needed
     loadTemplates(['modules/masks-newgeneration-unofficial/templates/shift-labels.hbs']);
-    loadTemplates(['modules/masks-newgeneration-unofficial/templates/influence-tab.hbs']);
-    loadTemplates(['modules/masks-newgeneration-unofficial/templates/influence-sheet.hbs']);
+    loadTemplates(['modules/masks-newgeneration-unofficial/templates/influences-tab.hbs']);
+    loadTemplates(['modules/masks-newgeneration-unofficial/templates/influences-tab-page.hbs']);
 });
 
 Hooks.once('pbtaSheetConfig', () => {
@@ -60,7 +60,7 @@ Hooks.once('pbtaSheetConfig', () => {
 Hooks.on("preCreateActor", async function (document, data, options, userId) {
     
     if (document.type === 'character') {
-    
+        document.updateSource({'flags.masks-newgeneration-unofficial.influences': []});
         // Add template for Description Tab's input box.
         // document.updateSource({
         //     'system.details.biography.value': game.i18n.localize('MASKS-SHEETS.Background.CustomTemplate')
@@ -84,33 +84,96 @@ Hooks.on("renderActorSheet", async (app, html, context) => {
             onLabelShiftClick(app.actor, html);
         }
 
-        // Remove influence
-        // const influenceEl = html[0].querySelector('.pbta.sheet.actor .cell--attributes-left .cell--influence');
-        // if (influenceEl) {
-        //     influenceEl.remove();
-        // }
-
-        // remove equipment
-        const equipmentEl = html[0].querySelector('.pbta.sheet.actor .sheet-main [data-tab=equipment]');
-        if (equipmentEl) {
-            equipmentEl.remove();
+        // rename equipment tab
+        const equipmentTabEl = html[0].querySelector('.pbta.sheet.actor .sheet-main [data-tab=equipment]');
+        if (equipmentTabEl) {
+            equipmentTabEl.innerText = game.i18n.localize("MASKS-SHEETS.Influences");
+            
         }
 
-        // Add the influence tab
-        // const tabsEl = html[0].querySelector('.pbta.sheet.actor .sheet-main .sheet-tabs');
-        // if (tabsEl) {
-        //     const influenceTabTemplate = _templateCache['modules/masks-newgeneration-unofficial/templates/influence-tab.hbs'](context, {allowProtoMethodsByDefault: true, allowProtoPropertiesByDefault: true});
-        //     tabsEl.insertAdjacentHTML('beforeend', influenceTabTemplate);
-        // }
-
         // Add the influence sheet-tab
-        // const sheetBodyEl = html[0].querySelector('.pbta.sheet.actor .sheet-main .sheet-body');
-        // if (sheetBodyEl) {
-        //     const influenceSheetTemplate = _templateCache['modules/masks-newgeneration-unofficial/templates/influence-sheet.hbs'](context, {allowProtoMethodsByDefault: true, allowProtoPropertiesByDefault: true});
-        //     sheetBodyEl.insertAdjacentHTML('beforeend', influenceSheetTemplate);
-        // }
+        const equipmentEl = html[0].querySelector('.pbta.sheet.actor .sheet-main .sheet-body .equipment .cell--equipment');
+        if (equipmentEl) {
+            const influenceSheetTemplate = _templateCache['modules/masks-newgeneration-unofficial/templates/influences-tab-page.hbs'](context, {allowProtoMethodsByDefault: true, allowProtoPropertiesByDefault: true});
+            equipmentEl.insertAdjacentHTML('beforebegin', influenceSheetTemplate);
+        }
+
+        // Create influence list and enable influence tab interactivity
+        onInfluenceCreate(app.actor, html);
+        html.find('.influence--name').on('change', async function(event) {
+            onInfluenceEdit(app.actor, event);
+        });
+        onInfluenceAction(app.actor, html);
     }
 });
+
+function onInfluenceCreate(actor, html) {
+    let create = html.find('.influence-create');
+    if (!create) { return; }
+    create.click(async function(event) {
+        event.preventDefault();
+
+        let item = {
+            "id": foundry.utils.randomID(),
+            "name": "",
+            "hasInfluenceOver": false,
+            "haveInfluenceOver": false,
+            "locked": false
+        }
+
+        let influences = actor.getFlag("masks-newgeneration-unofficial", "influences");
+        influences.push(item);
+        actor.setFlag("masks-newgeneration-unofficial", "influences", influences);
+    });
+}
+
+async function onInfluenceEdit(actor, event) {
+    event.preventDefault();
+    let influenceID = $(event.target).parents("[data-influence-id]").data().influenceId;
+    let influences = actor.getFlag("masks-newgeneration-unofficial", "influences");
+    let influence = influences.find(i => i.id === influenceID);
+    influence.name = event.target.value;
+
+    actor.setFlag("masks-newgeneration-unofficial", "influences", influences);
+}
+
+function onInfluenceAction(actor, html) {
+    let action = html.find('[data-influence-action]');
+    if (!action) { return; }
+    action.click(async function(event) {
+        event.preventDefault();
+        
+        const clickedElement = $(event.currentTarget);
+        const action = clickedElement.data().influenceAction;
+        let influenceID = clickedElement.parents("[data-influence-id]").data().influenceId;
+        let influences = actor.getFlag("masks-newgeneration-unofficial", "influences");
+        let influence = influences.find(i => i.id === influenceID);
+
+        if (influence.locked && /lock|roll/.exec(action) === null) {
+            return;
+        }
+
+        switch (action) {
+            case "hasInfluenceOver":
+                influence.hasInfluenceOver = !influence.hasInfluenceOver;
+                break;
+            case "haveInfluenceOver":
+                influence.haveInfluenceOver = !influence.haveInfluenceOver;
+                break;
+            case "lock":
+                influence.locked = !influence.locked;
+                break;
+            case "delete":
+                influences = influences.filter(i => i.id !== influence.id);
+                break;
+            default:
+                break;
+        }
+
+        await actor.setFlag("masks-newgeneration-unofficial", "influences", influences);
+    });
+}
+
 
 function onLabelShiftClick(actor, html) {
     let shiftRoll = html.find('.masks-shift-roll');
@@ -156,6 +219,5 @@ function onLabelShiftClick(actor, html) {
         if (performShift) { 
             await actor.update(statUpdate); 
         }
-    });
-    
+    });   
 }
