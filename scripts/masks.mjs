cebef76dcd6cbcdc8e0e50aac78d59d80fb26d6e
Joseph Hopson <jh@josephhopson.com>
2024-05-07 12:51:21 -0600

    fixed a v12 issue with influence

--- scripts/masks.mjs
+++ scripts/masks.mjs
@@ -26,7 +26,6 @@ Hooks.once("init", () => {
 
 Hooks.on('setup', () => {
     // Ensure template is loaded so that it will be ready when needed
-    loadTemplates(['modules/masks-newgeneration-unofficial/templates/influences-tab.hbs']);
     loadTemplates(['modules/masks-newgeneration-unofficial/templates/influences-tab-page.hbs']);
 });
 
@@ -67,7 +66,8 @@ Hooks.on("renderActorSheet", async (app, html, context) => {
         // Add the influence sheet-tab
         const equipmentEl = html[0].querySelector('.pbta.sheet.actor .sheet-main .sheet-body .equipment .cell--equipment');
         if (equipmentEl) {
-            const influenceSheetTemplate = _templateCache['modules/masks-newgeneration-unofficial/templates/influences-tab-page.hbs'](context, {allowProtoMethodsByDefault: true, allowProtoPropertiesByDefault: true});
+            // const influenceSheetTemplate = _templateCache['modules/masks-newgeneration-unofficial/templates/influences-tab-page.hbs'](context, {allowProtoMethodsByDefault: true, allowProtoPropertiesByDefault: true});
+            const influenceSheetTemplate = await renderTemplate('modules/masks-newgeneration-unofficial/templates/influences-tab-page.hbs', context);
             equipmentEl.insertAdjacentHTML('beforebegin', influenceSheetTemplate);
         }
 
@@ -78,7 +78,17 @@ Hooks.on("renderActorSheet", async (app, html, context) => {
         });
         onInfluenceAction(app.actor, html);
     }
+
+    let repList = [
+        {
+            "name": "faction name",
+            "neg-rep": 4,
+            "pos-rep": 2
+        }
+    ]
 });
+
+
 
 function onInfluenceCreate(actor, html) {
     let create = html.find('.influence-create');
