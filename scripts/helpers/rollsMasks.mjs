071c1d46e716bf56e33a0ceb35abe8b67f42ae9b
Joseph Hopson <jh@josephhopson.com>
2024-06-12 20:59:49 -0600

    todo update

--- scripts/helpers/rollsMasks.mjs
+++ scripts/helpers/rollsMasks.mjs
@@ -1,3 +1,16 @@
 export class RollPbtAMasks extends pbta.dice.RollPbtA {
+    /** @override */
+    async configureDialog() {
+        console.log('TESTING configureDialog');
+        console.log(this);
+        // console.log(this.data);
+        return super.configureDialog({ situationalMods: true });
+    }
 
+    /** @override */
+    _onDialogSubmit(html, stat) {
+        console.log('TESTING _onDialogSubmit');
+        console.log(this);
+        return super._onDialogSubmit();
+    }
 }
